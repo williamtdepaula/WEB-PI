892e7d4f7c5fd086ad5681f90397339c206895e6
William Tristão de Paula <william.depaula@agilecontent.com>
2021-10-28 23:02:52 -0300

    Heroku

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import Register from './pages/register';
 import {
   BrowserRouter as Router,
   Route,
+  HashRouter
 } from "react-router-dom";
 import ListPeople from './pages/list_people';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
         <Route component={Register}  path="/" exact />
         <Route component={ListPeople}  path="/relatorio" />
-    </Router>
+    </HashRouter>
   );
 }
 
