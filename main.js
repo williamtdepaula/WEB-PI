99b99e7386ea794cd982e6bfcebee5004f9b3e02
William Tristão de Paula <william.depaula@agilecontent.com>
2021-10-28 21:36:01 -0300

    Heroku

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const app = express()
 
 let listenPort = process.env.PORT || 5000;
 
+app.use(express.static("build"))
 app.listen(listenPort, ()=> {
   console.log("server running on port "+ listenPort);
 })
